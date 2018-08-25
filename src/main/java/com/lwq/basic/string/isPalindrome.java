431244fe1e773e17fd2d3659a9136a10d542a288
Lwq <943904454@qq.com>
2018-08-25 17:36:43 +0800

    判断一个字符串是不是回文串（只看字符串中的字母和数字）

---
+++ src/main/java/com/lwq/basic/string/isPalindrome.java
@@ -0,0 +1,59 @@
+package com.lwq.basic.string;
+
+/**
+ * @Author: Lwq
+ * @Date: 2018/8/25 17:07
+ * @Version 1.0
+ * @Describe  验证回文字符串
+ */
+public class isPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+        int first = 0;
+        int last = chars.length-1;
+        while (first<last){
+            if(!Character.isDigit(chars[first])&&!Character.isLetter(chars[first])){
+                first++;
+                continue;
+            }
+            if(!Character.isDigit(chars[last])&&!Character.isLetter(chars[last])){
+                last--;
+                continue;
+            }
+            if(chars[first]!=chars[last]){
+                return false;
+            }
+            first++;
+            last--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome1(String s) {
+        s = s.toLowerCase();
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < s.length();i++){
+            if((s.charAt(i)>='a'&&s.charAt(i)<='z' )||(s.charAt(i)>='0'&&s.charAt(i)<='9'))
+                result.append(s.charAt(i));
+        }
+        if(result.length()==0){
+            return true;
+        }
+        int start = 0;
+        int end = result.length()-1;
+        while (start<end){
+            if(result.charAt(start)!=result.charAt(end)){
+                return false;
+            }else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
