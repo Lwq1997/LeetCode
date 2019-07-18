df3eb74065c64ccfadf2e672bf9d72e331a056c3
lwq <liwanqi@guazi.com>
2019-07-18 22:10:15 +0800

    有效的字母异位词

---
+++ src/main/java/com/lwq/primary_algorithm/string/isAnagram.java
@@ -0,0 +1,56 @@
+package com.lwq.primary_algorithm.string;
+
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1:
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+示例 2:
+
+输入: s = "rat", t = "car"
+输出: false
+说明:
+你可以假设字符串只包含小写字母。
+
+进阶:
+如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class isAnagram {
+    public static void main(String[] args) {
+        String s = "a";
+        String t = "b";
+
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                if (0 == map.get(t.charAt(i))) {
+                    map.remove(t.charAt(i));
+                }
+            }
+
+        }
+
+        return map.size() == 0;
+    }
+
+
+}
