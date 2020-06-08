0952a5c8e11dfd4ceb6264e35e4030755c5e82ce
lwq <liwanqi@guazi.com>
2020-06-08 17:27:17 +0800

    435 无重叠区间

---
+++ src/main/java/com/lwq/LeetCode435.java
@@ -0,0 +1,64 @@
+package com.lwq;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ *给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+ *
+ * 注意:
+ *
+ * 可以认为区间的终点总是大于它的起点。
+ * 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+ * 示例 1:
+ *
+ * 输入: [ [1,2], [2,3], [3,4], [1,3] ]
+ *
+ * 输出: 1
+ *
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ * 示例 2:
+ *
+ * 输入: [ [1,2], [1,2], [1,2] ]
+ *
+ * 输出: 2
+ *
+ * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+ * 示例 3:
+ *
+ * 输入: [ [1,2], [2,3] ]
+ *
+ * 输出: 0
+ *
+ * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+ */
+public class LeetCode435 {
+    public int intervalSchedule(int[][] intvs) {
+        if (intvs.length == 0) {
+            return 0;
+        }
+        // 按end升序排列
+        Arrays.sort(intvs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        // 至少有一个区间不相交
+        int count = 1;
+        // 排序后，第一个区间就是 x
+        int x_end = intvs[0][1];
+
+        for (int[] intv : intvs) {
+            if(intv[0] >= x_end){
+                count++;
+                x_end = intv[1];
+            }
+        }
+        return count;
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        return intervals.length - intervalSchedule(intervals);
+    }
+}
