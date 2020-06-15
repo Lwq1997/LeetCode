0c52f4dcd9fb5e224c0b654b01fbc5d50badd9ef
lwq <liwanqi@guazi.com>
2020-06-15 16:34:46 +0800

    138 复制带随机指针的链表

---
+++ src/main/java/com/lwq/LeetCode138.java
@@ -0,0 +1,61 @@
+package com.lwq;
+
+/**
+ * 给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+ *
+ * 要求返回这个链表的 深拷贝。 
+ *
+ * 我们用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ *
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ */
+public class LeetCode138 {
+    public Node copyRandomList(Node head) {
+        if(head == null){
+            return null;
+        }
+        Node p = head;
+        // 第一步，复制源节点。
+        while (p != null){
+            Node p_clone = new Node(p.val);
+            Node temp = p.next;
+            p.next = p_clone;
+            p_clone.next = temp;
+            p = temp;
+        }
+        // 第二步，生成克隆节点的 random 指针。
+        p = head;
+        while (p != null){
+            if(p.random!=null){
+                p.next.random = p.random.next;
+            }
+            p = p.next.next;
+        }
+        // 第三步，拆分
+        p = head;
+        Node cloneHead = p.next;
+        Node cloneP = cloneHead;
+        while (cloneP.next != null) {
+            //原链表
+            p.next = p.next.next;
+            p=p.next;
+            //克隆链表
+            cloneP.next = cloneP.next.next;
+            cloneP = cloneP.next;
+        }
+        //最后不要忘了给原链表收尾
+        p.next = null;
+        return cloneHead;
+    }
+    class Node{
+        int val;
+        Node next;
+        Node random;
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
