5b549d3fe1bdaa9e60b32e2b2f8608a1a4cf89d6
liwanqi.gold <liwanqi.gold@bytedance.com>
2020-11-02 14:32:35 +0800

    LeetCode349. 两个数组的交集

---
+++ src/main/java/com/lwq/LeetCode349.java
@@ -0,0 +1,48 @@
+package com.lwq;
+
+import java.util.HashSet;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ */
+public class LeetCode349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                set2.add(i);
+            }
+        }
+        int[] res = new int[set2.size()];
+        int start = 0;
+        for (Integer integer : set2) {
+            res[start++] = integer;
+        }
+        return res;
+    }
+}
