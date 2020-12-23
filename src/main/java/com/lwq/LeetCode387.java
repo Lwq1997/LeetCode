ae4a90ebbb03427b4ce81d5561b7051b28df5167
liwanqi.gold <liwanqi.gold@bytedance.com>
2020-12-23 21:34:01 +0800

    LeetCode387. 字符串中的第一个唯一字符

---
+++ src/main/java/com/lwq/LeetCode387.java
@@ -0,0 +1,37 @@
+package com.lwq;
+
+/**
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * s = "leetcode"
+ * 返回 0
+ *
+ * s = "loveleetcode"
+ * 返回 2
+ *  
+ *
+ * 提示：你可以假定该字符串只包含小写字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode387 {
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            count[c-'a'] += 1;
+        }
+        for(int i = 0; i < chars.length; i++){
+            if(count[chars[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
