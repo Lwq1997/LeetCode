0dbbc96cc8878fa8910a51f9bd4dd96339cf0e55
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-04-23 21:17:05 +0800

    无重复字符的最长子串-优化滑动窗口

--- src/main/java/com/lwq/LeetCode003.java
+++ src/main/java/com/lwq/LeetCode003.java
@@ -25,7 +25,44 @@ import java.util.HashMap;
  *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  */
 public class LeetCode003 {
-    public int lengthOfLongestSubstring(String s) {
+    public static void main(String[] args) {
+        LeetCode003 leetCode003 = new LeetCode003();
+        System.out.println(leetCode003.lengthOfLongestSubstring01("abcabcbb"));
+        System.out.println(leetCode003.lengthOfLongestSubstring02("abcabcbb"));
+    }
+
+    /**
+     * 滑动窗口
+     * 左边界为
+     * 有边界为i
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring01(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //判断C是否出现过
+            if (map.containsKey(c)) {
+                //C当时的下标
+                Integer index = map.get(c);
+                left = Math.max(left, index + 1);
+            }
+            //把字符填充进去
+            map.put(c, i);
+            res = Math.max(res, i - left + 1);
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring02(String s) {
         HashMap<Character, Integer> window = new HashMap<>();
         char[] chars = s.toCharArray();
 
