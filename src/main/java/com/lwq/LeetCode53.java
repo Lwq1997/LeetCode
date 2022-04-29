335c96592c2dd3fd735f3ec519b43edf127efadc
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-04-29 17:42:09 +0800

    53，返回最大和的连续子数组

--- src/main/java/com/lwq/LeetCode53.java
+++ src/main/java/com/lwq/LeetCode53.java
@@ -14,7 +14,9 @@ package com.lwq;
  */
 public class LeetCode53 {
     public static void main(String[] args) {
-
+        LeetCode53 leetCode53 = new LeetCode53();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(leetCode53.maxSubArray(nums));
     }
 
     /**
