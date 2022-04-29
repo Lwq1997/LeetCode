2f83c6b20542931348194e0b9c4ac668a046e1e2
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-04-29 18:56:32 +0800

    22，链表中倒数第K个节点

---
+++ src/main/java/com/lwq/LeetCode022.java
@@ -0,0 +1,43 @@
+package com.lwq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ *
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ *
+ * 返回链表 4->5.
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author lwq
+ */
+public class LeetCode022 {
+    public static void main(String[] args) {
+        LeetCode022 leetCode022 = new LeetCode022();
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        System.out.println(leetCode022.getKthFromEnd(listNode, 2).val);
+    }
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        return list.get(list.size() - k);
+    }
+}
