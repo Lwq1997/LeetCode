0728f1d78893c5fe7e87fe2ff706476ae116803f
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-04-29 20:11:39 +0800

    179：最大数

---
+++ src/main/java/com/lwq/LeetCode179.java
@@ -0,0 +1,56 @@
+package com.lwq;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+ * <p>
+ * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,2]
+ * 输出："210"
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,30,34,5,9]
+ * 输出："9534330"
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/largest-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode179 {
+    public static void main(String[] args) {
+        //测试
+        LeetCode179 leetCode179 = new LeetCode179();
+        int[] nums = {10, 2};
+        System.out.println(leetCode179.largestNumber(nums));
+    }
+
+    public String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str);
+        }
+        String res = sb.toString();
+        if (res.charAt(0) == '0') {
+            return "0";
+        }
+        return res;
+    }
+}
