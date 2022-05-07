237d2a7fec094897ee20d3807a8e3ea5659e6094
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-05-07 17:28:38 +0800

    226 翻转二叉树

---
+++ src/main/java/com/lwq/LeetCode226.java
@@ -0,0 +1,122 @@
+package com.lwq;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ * 示例 3：
+ *
+ * 输入：root = []
+ * 输出：[]
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode226 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        LeetCode226 leetCode226 = new LeetCode226();
+        TreeNode root1 = leetCode226.invertTree(root);
+        TreeNode root2 = leetCode226.invertTreeByQueue(root);
+        TreeNode root3 = leetCode226.invertTreeByStack(root);
+    }
+    /**
+     * 递归方式遍历反转
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    /**
+     * 层序遍历方式反转
+     */
+    public TreeNode invertTreeByQueue(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+
+    /**
+     * 深度优先遍历的方式反转
+     */
+    private TreeNode invertTreeByStack(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = stack.pop();
+                TreeNode temp = cur.left;
+                cur.left = cur.right;
+                cur.right = temp;
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
+            }
+        }
+        return root;
+    }
+}
