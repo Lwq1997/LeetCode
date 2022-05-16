11d32dbc9c921afc2b0eded1567b8bfd7300d6c0
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-05-16 16:10:02 +0800

    32. 最长有效括号

---
+++ src/main/java/com/lwq/LeetCode32.java
@@ -0,0 +1,60 @@
+package com.lwq;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ * 示例 2：
+ * <p>
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ * 示例 3：
+ * <p>
+ * 输入：s = ""
+ * 输出：0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode32 {
+
+    @Test
+    public void test() {
+        String s = ")()())";
+        System.out.println(longestValidParentheses(s));
+    }
+
+    //我们始终保持栈[底]元素为当前已经遍历过的元素中「最后一个没有被匹配的右括号的下标」
+    public int longestValidParentheses(String s) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    //只有空栈才会进入右括号
+                    stack.push(i);
+                } else {
+                    res = Math.max(res, i - stack.peek());
+                }
+            }
+        }
+        return res;
+    }
+}
