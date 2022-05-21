e87859294970ecfdd20e01a84e765b5fdf3712e6
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-05-21 21:07:10 +0800

    142. 环形链表 II

--- src/main/java/com/lwq/LeetCode142.java
+++ src/main/java/com/lwq/LeetCode142.java
@@ -2,73 +2,69 @@ package com.lwq;
 
 /**
  * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
- *
+ * <p>
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
- *
+ * <p>
  * 说明：不允许修改给定的链表。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：head = [3,2,0,-4], pos = 1
  * 输出：tail connects to node index 1
  * 解释：链表中有一个环，其尾部连接到第二个节点。
- *
- *
+ * <p>
+ * <p>
  * 示例 2：
- *
+ * <p>
  * 输入：head = [1,2], pos = 0
  * 输出：tail connects to node index 0
  * 解释：链表中有一个环，其尾部连接到第一个节点。
- *
- *
+ * <p>
+ * <p>
  * 示例 3：
- *
+ * <p>
  * 输入：head = [1], pos = -1
  * 输出：no cycle
  * 解释：链表中没有环。
- *
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
+ * <p>
  * 进阶：
  * 你是否可以不用额外空间解决此题？
  */
 public class LeetCode142 {
     public static void main(String[] args) {
-
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        LeetCode142 leetCode142 = new LeetCode142();
+        ListNode res = leetCode142.detectCycle(head);
+        System.out.println(res.val);
     }
 
     public ListNode detectCycle(ListNode head) {
-        if (head == null) {
+        if (head == null || head.next == null) {
             return null;
         }
-        ListNode res = getIntersect(head);
-        if (res == null) {
-            return null;
-        }
-
-        ListNode h1 = head;
-        ListNode h2 = res;
-        while (h1 != h2) {
-            h1 = h1.next;
-            h2 = h2.next;
-        }
-        return h1;
-    }
-
-    private ListNode getIntersect(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) {
-                return slow;
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null || fast.next.next == null) {
+                return null;
             }
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        return null;
+        slow = head;
+        while (slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
