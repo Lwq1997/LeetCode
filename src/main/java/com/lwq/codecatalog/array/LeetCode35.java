391e3e33a7ba6e2bd27e81d84efbc5711660aff0
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-05-30 19:29:32 +0800

    35. 搜索插入位置

---
+++ src/main/java/com/lwq/codecatalog/array/LeetCode35.java
@@ -0,0 +1,93 @@
+package com.lwq.codecatalog.array;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ * 示例 3:
+ * <p>
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ *  
+ * <p>
+ * 提示:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums 为 无重复元素 的 升序 排列数组
+ * -104 <= target <= 104
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode35 {
+    public static void main(String[] args) {
+        LeetCode35 leetCode35 = new LeetCode35();
+        int[] nums = {1, 2, 4, 5};
+        int target = 3;
+        System.out.println(leetCode35.searchInsert01(nums, target));
+        System.out.println(leetCode35.searchInsert02(nums, target));
+    }
+
+    /**
+     * 暴力解法
+     * 四种场景：
+     * 目标值在数组所有元素之前
+     * 目标值等于数组中某一个元素
+     * 目标值插入数组中的位置
+     * 目标值在数组所有元素之后
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert01(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public int searchInsert02(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            //开始二分，区间左闭右闭
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // 分别处理如下四种情况
+        // 目标值在数组所有元素之前  [0, -1]
+        // 目标值等于数组中某一个元素  return middle;
+        // 目标值插入数组中的位置 [left, right]，return  right + 1
+        // 目标值在数组所有元素之后的情况 [left, right]， return right + 1
+        return right + 1;
+    }
+}
