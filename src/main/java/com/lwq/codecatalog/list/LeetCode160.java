f587e78d578df999b8404136c3cf235de9b9da97
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-06-11 20:03:51 +0800

    160. 相交链表

---
+++ src/main/java/com/lwq/codecatalog/list/LeetCode160.java
@@ -0,0 +1,45 @@
+package com.lwq.codecatalog.list;
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * <p>
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
+ * <p>
+ * <p>
+ * 题目数据 保证 整个链式结构中不存在环。
+ * <p>
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/intersection-of-two-linked-lists-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // A和B两个链表长度可能不同，但是A+B和B+A的长度是相同的
+        // 所以遍历A+B和遍历B+A一定是同时结束。
+        // 如果A,B相交的话A和B有一段尾巴是相同的，所以两个遍历的指针一定会同时到达交点
+        // 如果A,B不相交的话两个指针就会同时到达A+B（B+A）的尾节点
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 != null ? p1.next : headB;
+            p2 = p2 != null ? p2.next : headA;
+        }
+        return p1;
+    }
+}
