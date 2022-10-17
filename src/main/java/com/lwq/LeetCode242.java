658e3ec67a09b94ceea7343be5559d6bd8bea0d8
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-17 21:23:11 +0800

    242.有效的字母异位词

--- src/main/java/com/lwq/LeetCode242.java
+++ src/main/java/com/lwq/LeetCode242.java
@@ -29,61 +29,29 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetCode242 {
-    public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> s1 = new HashMap<>();
-        HashMap<Character, Integer> s2 = new HashMap<>();
-        char[] chars1 = s.toCharArray();
-        char[] chars2 = t.toCharArray();
-        for (char c : chars1) {
-            s1.put(c, s1.getOrDefault(c, 0) + 1);
-        }
-        for (char c : chars2) {
-            s2.put(c, s2.getOrDefault(c, 0) + 1);
-        }
-        return s1.equals(s2);
-    }
+    /**
+     * 242. 有效的字母异位词 字典解法
+     * 时间复杂度O(m+n) 空间复杂度O(1)
+     */
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            int[] record = new int[26];
 
-    public boolean isAnagram_1(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        return String.valueOf(sChars).equals(String.valueOf(tChars));
-    }
-
-    public boolean isAnagram_2(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for (char ch : t.toCharArray()) {
-            Integer count = map.get(ch);
-            if (count == null) {
-                return false;
-            } else if (count > 1) {
-                map.put(ch, count - 1);
-            } else {
-                map.remove(ch);
+            for (int i = 0; i < s.length(); i++) {
+                record[s.charAt(i) - 'a']++;
             }
-        }
-        return map.isEmpty();
-    }
 
-    public boolean isAnagram3(String s, String t) {
-        int[] sCounts = new int[26];
-        int[] tCounts = new int[26];
-        for (char ch : s.toCharArray()) {
-            sCounts[ch - 'a']++;
-        }
-        for (char ch : t.toCharArray()) {
-            tCounts[ch - 'a']++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (sCounts[i] != tCounts[i]) {
-                return false;
+            for (int i = 0; i < t.length(); i++) {
+                record[t.charAt(i) - 'a']--;
             }
+
+            for (int count: record) {
+                if (count != 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
 }
 
