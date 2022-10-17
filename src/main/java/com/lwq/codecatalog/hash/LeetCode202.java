a9f4b53e301eff0de0bd4d0d065b9c6385ccc06b
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-17 21:42:18 +0800

    第202题. 快乐数

---
+++ src/main/java/com/lwq/codecatalog/hash/LeetCode202.java
@@ -0,0 +1,56 @@
+package com.lwq.codecatalog.hash;
+
+import java.util.HashSet;
+
+/**
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * <p>
+ * 「快乐数」 定义为：
+ * <p>
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * 示例 2：
+ * <p>
+ * 输入：n = 2
+ * 输出：false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/happy-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode202 {
+    public boolean isHappy(int n) {
+        //用来记录中间出现过的数字
+        HashSet<Integer> record = new HashSet<>();
+        while (n != 1 && !record.contains(n)) {
+            //如果退出，说明可能循环了，也可能找到快乐数
+            record.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    private int getNextNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n = n / 10;
+        }
+        return res;
+    }
+}
