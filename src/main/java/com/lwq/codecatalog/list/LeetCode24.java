3e160774890e200bcf04be388155cf69160bb51e
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-17 16:54:01 +0800

    24. 两两交换链表中的节点

--- src/main/java/com/lwq/codecatalog/list/LeetCode24.java
+++ src/main/java/com/lwq/codecatalog/list/LeetCode24.java
@@ -24,23 +24,48 @@ package com.lwq.codecatalog.list;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetCode24 {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode listNode1 = swapPairs02(listNode);
+    }
     /**
      * 递归版本
      * @param head
      * @return
      */
-    public ListNode swapPairs(ListNode head) {
+    public static ListNode swapPairs01(ListNode head) {
         if(head == null || head.next==null){
             return head;
         }
         ListNode next = head.next;
         // 进行递归
-        ListNode newNode = swapPairs(next.next);
+        ListNode newNode = swapPairs01(next.next);
         // 这里进行交换，仅仅需要交换两个节点
         next.next = head;
         head.next = newNode;
 
         //这里返回两个节点中的头结点，也就是next
         return next;
+    }
+
+    /**
+     * 虚拟头节点版本
+     * @param head
+     * @return
+     */
+    public static ListNode swapPairs02(ListNode head) {
+        ListNode dummy = new ListNode(0,head);
+        ListNode pre = dummy;
+        while(pre.next!=null && pre.next.next!=null) {
+            //说明有两个可以翻转的节点存在
+            ListNode temp = head.next.next; // 先把下一次翻转的节点存下来，以防丢失
+
+            pre.next = head.next; //步骤一
+            head.next.next = head; // 步骤二
+            head.next = temp;  // 步骤三
+            pre = head;
+            head = head.next;
+        }
+        return dummy.next;
     }
 }
