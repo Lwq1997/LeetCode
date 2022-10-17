6adc97b149dfee3db780563c33615641ebc9a0e0
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-17 21:32:01 +0800

    349. 两个数组的交集

---
+++ src/main/java/com/lwq/codecatalog/hash/LeetCode349.java
@@ -0,0 +1,45 @@
+package com.lwq.codecatalog.hash;
+
+import java.util.HashSet;
+
+/**
+ * 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * 解释：[4,9] 也是可通过的
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/intersection-of-two-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> resSet = new HashSet<>();
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                resSet.add(i);
+            }
+        }
+        return resSet.stream().mapToInt(x -> x).toArray();
+    }
+}
