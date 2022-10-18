f10d01022b9b6916bac58174bcd52718dfd2669a
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-18 15:50:57 +0800

    #151.翻转字符串里的单词

---
+++ src/main/java/com/lwq/codecatalog/string/LeetCode151.java
@@ -0,0 +1,112 @@
+package com.lwq.codecatalog.string;
+
+/**
+ * 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+ * <p>
+ * 单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+ * <p>
+ * 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+ * <p>
+ * 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "the sky is blue"
+ * 输出："blue is sky the"
+ * 示例 2：
+ * <p>
+ * 输入：s = "  hello world  "
+ * 输出："world hello"
+ * 解释：反转后的字符串中不能存在前导空格和尾随空格。
+ * 示例 3：
+ * <p>
+ * 输入：s = "a good   example"
+ * 输出："example good a"
+ * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 包含英文大小写字母、数字和空格 ' '
+ * s 中 至少存在一个 单词
+ *  
+ * <p>
+ * 进阶：如果字符串在你使用的编程语言中是一种可变数据类型，请尝试使用 O(1) 额外空间复杂度的 原地 解法。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/reverse-words-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode151 {
+    /**
+     * 不使用Java内置方法实现
+     * <p>
+     * 1.去除首尾以及中间多余空格
+     * 2.反转整个字符串
+     * 3.反转各个单词
+     */
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        //1.去除首尾以及中间多余空格
+        chars = removeExtraSpaces(chars);
+        //2.整个字符串反转
+        reverse(chars, 0, chars.length - 1);
+        //3.单词反转
+        reverseEachWord(chars);
+        return new String(chars);
+    }
+
+    private void reverseEachWord(char[] chars) {
+        int start = 0;
+        //end <= s.length() 这里的 = ，是为了让 end 永远指向单词末尾后一个位置，这样 reverse 的实参更好设置
+        for (int end = 0; end <= chars.length; end++) {
+            if (end == chars.length || chars[end] == ' ') {
+                // end 每次到单词末尾后的空格或串尾,开始反转单词
+                reverse(chars, start, end - 1);
+                start = end + 1;
+            }
+        }
+    }
+
+    private void reverse(char[] chars, int left, int right) {
+        if (right >= chars.length) {
+            System.out.println("set a wrong right");
+            return;
+        }
+        while (left < right) {
+            chars[left] ^= chars[right];
+            chars[right] ^= chars[left];
+            chars[left] ^= chars[right];
+            left++;
+            right--;
+        }
+    }
+
+    private char[] removeExtraSpaces(char[] chars) {
+        int slow = 0;
+        for (int fast = 0; fast < chars.length; fast++) {
+            //先用fast移除空格
+            if (chars[fast] != ' ') {
+                //在用 slow 加空格。 除第一个单词外，单词末尾要加空格
+                if (slow != 0) {
+                    chars[slow++] = ' ';
+                }
+                //fast 遇到空格或遍历到字符串末尾，就证明遍历完一个单词了
+                while (fast < chars.length && chars[fast] != ' ') {
+                    chars[slow++] = chars[fast++];
+                }
+            }
+//            else {
+//                fast++;
+//            }
+        }
+        char[] newChars = new char[slow];
+        System.arraycopy(chars, 0, newChars, 0, slow);
+        return newChars;
+    }
+}
