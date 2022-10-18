b5c4b542e0fa2ccd0b6f30b24247d4552cc159d6
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-18 15:55:32 +0800

    #题目：剑指Offer58-II.左旋转字符串

---
+++ src/main/java/com/lwq/codecatalog/string/LeetCode58.java
@@ -0,0 +1,49 @@
+package com.lwq.codecatalog.string;
+
+/**
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ * <p>
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode58 {
+    public String reverseLeftWords01(String s, int n) {
+        return s.substring(n, s.length()) + s.substring(0, n);
+    }
+
+    /**
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords02(String s, int n) {
+        int len = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        reverseString(sb, 0, n - 1);
+        reverseString(sb, n, len - 1);
+        return sb.reverse().toString();
+    }
+
+    public void reverseString(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
