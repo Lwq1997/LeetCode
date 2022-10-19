da1f6c69528b96f8d7f025093b8f56bba5582a9b
liwanqi.gold <liwanqi.gold@bytedance.com>
2022-10-20 00:23:15 +0800

    #239. 滑动窗口最大值

---
+++ src/main/java/com/lwq/codecatalog/stack_queue/LeetCode239.java
@@ -0,0 +1,82 @@
+package com.lwq.codecatalog.stack_queue;
+
+
+import java.util.ArrayDeque;
+
+/**
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回 滑动窗口中的最大值 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/sliding-window-maximum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode239 {
+    public static void main(String[] args) {
+        maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
+    }
+
+    /**
+     * 使用一个双端队列
+     *
+     * 解释过程中队列中都是具体的值，方便理解，具体见代码。
+     * 初始状态：L=R=0,队列:{}
+     * i=0,nums[0]=1。队列为空,直接加入。队列：{1}
+     * i=1,nums[1]=3。队尾值为1，3>1，弹出队尾值，加入3。队列：{3}
+     * i=2,nums[2]=-1。队尾值为3，-1<3，直接加入。队列：{3,-1}。此时窗口已经形成，L=0,R=2，result=[3]
+     * i=3,nums[3]=-3。队尾值为-1，-3<-1，直接加入。队列：{3,-1,-3}。队首3对应的下标为1，L=1,R=3，有效。result=[3,3]
+     * i=4,nums[4]=5。队尾值为-3，5>-3，依次弹出后加入。队列：{5}。此时L=2,R=4，有效。result=[3,3,5]
+     * i=5,nums[5]=3。队尾值为5，3<5，直接加入。队列：{5,3}。此时L=3,R=5，有效。result=[3,3,5,5]
+     * i=6,nums[6]=6。队尾值为3，6>3，依次弹出后加入。队列：{6}。此时L=4,R=6，有效。result=[3,3,5,5,6]
+     * i=7,nums[7]=7。队尾值为6，7>6，弹出队尾值后加入。队列：{7}。此时L=5,R=7，有效。result=[3,3,5,5,6,7]
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            if (i >= k) {
+                //当滑动到下一个窗口的时候，需要判断一下队列头部的元素是不是我们想移除的那个元素
+                if (deque.peekFirst() == nums[i - k + 1]) {
+                    deque.removeFirst();
+                }
+            }
+            //如果当前元素比队列的最后一个元素大，那么就将最后一个元素出队，重复这步直到当前元素小于队列的最后一个元素或者队列为空。进行下一步。
+            while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            if (i >= k - 1) {
+                // 因为单调，当i增长到符合第一个k范围的时候，每滑动一步都将队列头节点放入结果就行了
+                res[index++] = deque.peekFirst();
+            }
+        }
+        return res;
+    }
+}
